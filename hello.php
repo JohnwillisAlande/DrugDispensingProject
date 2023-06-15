e29979059e7a4d8d506971ba95b1e1ef3bc555ec
TommyNdemo <tomndemo6@gmail.com>
2023-06-15 10:58:17 +0300

    UPLOAD 1

---
+++ hello.php
@@ -0,0 +1,12 @@
+<?php
+$data=["Game of Thrones"=>['Jamie ',"Cantie"],
+"Lisa Simsono"=>['Trix ',"Hitad"]];
+echo "<h2>$series</h2>";
+foreach ($data as $series => $actors) {
+    echo "<h2>$series</h2>";
+    foreach($actor as $actors){
+        echo"<div>$actorS</div>";
+    }
+}
+
+?>
