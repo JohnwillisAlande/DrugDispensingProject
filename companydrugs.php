324d1d42eacad8eb4cf14d03acf2a44ee81382b8
JohnwillisAlande <johnwillis.alande@strathmore.edu>
2023-07-17 15:30:10 +0300

    companydrugs

--- companydrugs.php
+++ companydrugs.php
@@ -1,24 +1,52 @@
 <?php
 require_once("connect.php");
-if (isset($_COOKIE['companyID']) && isset($_COOKIE['tradename']) && isset($_COOKIE['formula']) && isset($_COOKIE['price'])) {
-    $companyID = $_COOKIE['companyID'];
-    $tradename = $_COOKIE['tradename'];
-    $formula = $_COOKIE['formula'];
-    $price = $_COOKIE['price'];
 
-    // Store data in the 'drugs' table
-    $sql = "INSERT INTO drugs (companyID, tradename, formula, price) VALUES ('$companyID', '$tradename', '$formula', '$price')";
+$companyID = "";
+$tradename = "";
+$formula = "";
+$price = "";
 
-    if ($conn->query($sql) === TRUE) {
-        echo "Data stored successfully.";
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    // Validate and sanitize user inputs
+    $tradename = $_POST["tradename"] ?? "";
+    $formula = $_POST["formula"] ?? "";
+    $price = $_POST["price"] ?? "";
+
+    // Check if the inputs are not empty
+    if (empty($tradename) || empty($formula) || empty($price)) {
+        echo "Error: Please fill in all the fields.";
     } else {
-        echo "Error: " . $sql . "<br>" . $conn->error;
+        // Check if cookies are set and get values from cookies
+        if (isset($_COOKIE["email"])) {
+            $email = $_COOKIE["email"];
+
+            // Get the company ID from the database
+            $sql = "SELECT companyID FROM pharmaceuticalcompany WHERE email='$email'";
+            $result = $conn->query($sql);
+            if ($result->num_rows > 0) {
+                $row = $result->fetch_assoc();
+                $companyID = $row["companyID"];
+
+                // Store data in the 'drugs' table
+                $sql = "INSERT INTO drugs (companyID, tradename, formula, price) VALUES ('$companyID', '$tradename', '$formula', '$price')";
+
+                if ($conn->query($sql) === TRUE) {
+                    echo "Data stored successfully.";
+                } else {
+                    echo "Error: " . $sql . "<br>" . $conn->error;
+                }
+            } else {
+                echo "Error: Company ID not found in database.";
+            }
+        } else {
+            echo "Error: Company ID not found in cookies.";
+        }
     }
 }
 
 // Retrieve data from the 'drugs' table
-$sql = "SELECT * FROM drugs";
-$result = $conn->query($sql);
+$sql1 = "SELECT * FROM drugs";
+$result = mysqli_query($conn,$sql1);
 ?>
 
 <!DOCTYPE html>
@@ -26,16 +54,14 @@ $result = $conn->query($sql);
 
 <head>
     <title>Pharmaceutical Company Drugs</title>
+    <link rel="stylesheet" href="styles.css">
 </head>
 
 <body>
     <h1>Pharmaceutical Company Drugs</h1>
 
-    <form method="post" action="pharmaceuticalcompany.php">
-        <!-- Your HTML form for inputting companyID, tradename, formula, and price -->
-        <label for="companyID">Company ID:</label>
-        <input type="text" name="companyID" required><br>
-
+    <form method="post" action="companydrugs.php">
+        <!-- Your HTML form for inputting tradename, formula, and price -->
         <label for="tradename">Trade Name:</label>
         <input type="text" name="tradename" required><br>
 
@@ -50,7 +76,7 @@ $result = $conn->query($sql);
 
     <br>
     <h2>Drug Data Table</h2>
-    <table border="1">
+    <table>
         <tr>
             <th>Company ID</th>
             <th>Trade Name</th>
@@ -62,10 +88,10 @@ $result = $conn->query($sql);
         if ($result->num_rows > 0) {
             while ($row = $result->fetch_assoc()) {
                 echo "<tr>";
-                echo "<td>" . $row['companyID'] . "</td>";
-                echo "<td>" . $row['tradename'] . "</td>";
-                echo "<td>" . $row['formula'] . "</td>";
-                echo "<td>" . $row['price'] . "</td>";
+                echo "<td>" . $row["CompanyID"] . "</td>";
+                echo "<td>" . $row["TradeName"] . "</td>";
+                echo "<td>" . $row["Formula"] . "</td>";
+                echo "<td>" . $row["Price"] . "</td>";
                 echo "</tr>";
             }
         } else {
