a67ba90f672490084d15eb224ddd9d405da61dd4
TommyNdemo <tomndemo6@gmail.com>
2023-11-27 23:11:20 +0300

    API 2

--- javascriptServer/index.js
+++ javascriptServer/index.js
@@ -2,46 +2,63 @@ const connection = require("./conn");
 const express = require("express");
 const jwt = require("jsonwebtoken");
 const fs = require("fs");
-const bcrypt = requir("bcrypt");
+const bcrypt = require("bcrypt");
 
+const bodyParser = require("body-parser");
 const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 const conn = connection();
 
 // Secret key for signing and verifying the JWT
-const userAPIkey = "your-secret-key";
+const userkey = "AxSr3fa23gsh2vi9h";
 
 app.post("/SignIn", (req, res) => {
-  const { username, password } = req.body;
+  const { email, password } = req.body;
   let user;
 
   conn.query(
-    `SELECT * FROM APIUSERS WHERE USERNAME=${username}`,
+    `SELECT * FROM API_USERS WHERE Email="${email}"`,
     async (err, result) => {
       if (err) {
-        console.log(err);
+        res.send(err);
+        return console.log(err);
       } else {
         const passHash = result[0].Password;
-        const match = await bcrypt.compare(password, passHash);
-        if (match) {
-          user = result;
-        }
+        await bcrypt
+          .compare(password, passHash)
+          .then((match) => {
+            if (match) {
+              user = result;
+              console.log(match, user);
+            }
+          })
+          .then(() => {
+            if (user) {
+              console.log("xx");
+              // Generate a JWT token
+              const token = jwt.sign(
+                { userId: user.id, username: user.username },
+                userkey,
+                { expiresIn: "1h" }
+              );
+
+              res.send({ token, proceed: true, data: user });
+            } else {
+              res.send({ proceed: false });
+            }
+          });
       }
     }
   );
-  if (user) {
-    // Generate a JWT token
-    const token = jwt.sign(
-      { userId: user.id, username: user.username },
-      secretKey,
-      { expiresIn: "1h" }
-    );
-
-    res.send({ token, proceed: true });
-  } else {
-    res.send({ proceed: false });
-  }
 });
+
+// Generate a random 6-digit verification code
+const generateVerificationCode = () => {
+  return Math.floor(10000 + Math.random() * 90000);
+};
 
 app.post("/SignUp", async (req, res) => {
   const { name, username, password, email } = req.body;
@@ -51,34 +68,26 @@ app.post("/SignUp", async (req, res) => {
   const saltRounds = 12;
   const hashedPassword = await bcrypt.hash(password, saltRounds);
 
-  // Generate a random 6-digit verification code
-  const generateVerificationCode = () => {
-    return Math.floor(10000 + Math.random() * 90000);
-  };
-
   const id = generateVerificationCode();
 
   delete data.password;
   data.hashedPassword = hashedPassword;
 
-  conn.query(
-    `INSERT INTO APIUSERS (APIUSERID,Name, UserName, Email, Password) VALUES(${id},${name},${username},${email},${hashedPassword})`,
-    async (err, result) => {
-      if (err) {
-        console.log(err);
-      } else {
-        console.log(result);
-        user = data;
-      }
+  var sql = `INSERT INTO API_USERS (API_USER_ID, Name, UserName, Email, Password) VALUES(${id}, '${name}', '${username}', '${email}', '${hashedPassword}')`;
+
+  conn.query(sql, async (err, result) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(result);
+      user = data;
     }
-  );
+  });
   if (user) {
     // Generate a JWT token
-    const token = jwt.sign(
-      { userId: user.id, username: user.username },
-      secretKey,
-      { expiresIn: "1h" }
-    );
+    const token = jwt.sign({ userId: id, username: username }, userkey, {
+      expiresIn: "1h",
+    });
 
     res.send({ token, proceed: true, data: data });
   } else {
@@ -100,34 +109,27 @@ app.get("/Apis/Home", authenticateToken, (req, res) => {
   res.json({ message: "This is a protected resource" });
 });
 
+app.get("/SignIn", (req, res) => {
+  fs.readFile("../javascriptclient/SignIn.html", (err, result) => {
+    if (err) {
+      console.error(err);
+    } else {
+      res.send(result.toString());
+    }
+  });
+});
+
 function authenticateToken(req, res, next) {
   const token = req.header("Authorization");
 
   if (!token) return res.send({ message: "Unauthorized" });
 
-  jwt.verify(token, secretKey, (err, user) => {
+  jwt.verify(token, userkey, (err, user) => {
     if (err) return res.send({ message: "Forbidden" });
     req.user = user;
     next();
   });
 }
-
-app.get("/Hom", (req, res) => {
-  let file;
-  fs.readFile("../javascriptclient/Home.html", (err, result) => {
-    if (err) {
-      console.log(err);
-    } else {
-      file = result.toString();
-      res.send(file);
-      console.log(file);
-    }
-  });
-});
-
-app.get("/value", (req, res) => {
-  res.send({ key: "value" });
-});
 
 app.listen(4000, () => {
   console.error("listening on port 4000");
